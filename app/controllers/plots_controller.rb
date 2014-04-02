9b543b4ec914753d6338169446a1e9f24d7c8d01
supster <supatra.indhara@gmail.com>
2014-04-01 21:38:46 -0400

    style and redirect

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -15,4 +15,15 @@ class PlotsController < ApplicationController
 			render "edit"
 		end
 	end
+
+	def index
+		sort_column = "substr(code, 1,1), cast(substr(code, 2) as integer)"
+		@phases = Phase.all
+		
+		if !params.has_key?(:phase)
+			@plots = Plot.order(sort_column).all
+		else
+			@plots = Plot.where("phase_id = #{params[:phase]}").order(sort_column).all
+		end
+	end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def show
+  	redirect_to map_path($map_id) if signed_in?
   end
 end
